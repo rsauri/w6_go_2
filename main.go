43ba179d5422cae3c62f05f541c03acc63f2f603
rssauri <rosemariesauri@loyalistcollege.com>
2024-10-03 21:14:59 -0400

    Added /products POST handler

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ type PantryItem struct {
 
 // Initialize Variables
 var pantryItems []PantryItem
+var nextId int = 1
 
 // Method Handlers
 // GET All Pantry Items
@@ -35,6 +36,34 @@ func getItems(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(pantryItems)
 }
+
+// POST Create a new pantry item
+func createPantryItem(w http.ResponseWriter, r *http.Request) {
+	// Validate if the HTTP Method is correct
+	if r.Method != http.MethodPost {
+		http.Error(w, fmt.Sprintf("Invalid request method %s", r.Method), http.StatusMethodNotAllowed)
+		return
+	}
+
+	//Initialize Variable
+	var item PantryItem
+
+	//Get the item information from the request body and insert it to the list of items
+	json.NewDecoder(r.Body).Decode(&item)
+
+	//Add the new item to the Pantry Item List
+	item.ID = nextId
+	pantryItems = append(pantryItems, item)
+
+	//Increment the Id for the next Pantry Item
+	nextId++
+
+	//Write the response
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(item)
+
+}
+
 func main() {
 
 	//Handle the following /pantryItems methods
@@ -43,6 +72,8 @@ func main() {
 		switch r.Method {
 		case http.MethodGet:
 			getItems(w, r)
+		case http.MethodPost:
+			createPantryItem(w, r)
 		default:
 			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
 		}
